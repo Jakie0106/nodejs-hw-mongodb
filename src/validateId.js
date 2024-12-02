0f4000df10015557d1c3f1fc1dabb63410f0bae4
Jakie0106 <kamikadze777939@gmail.com>
2024-12-02 14:43:56 +0200

    hw-4

---
+++ src/validateId.js
@@ -0,0 +1,10 @@
+// validateId.js
+import mongoose from 'mongoose';
+
+const id = '67463aaccc4b62f3aa015f53';
+
+if (mongoose.Types.ObjectId.isValid(id)) {
+  console.log('ID is valid');
+} else {
+  console.log('ID is invalid');
+}
