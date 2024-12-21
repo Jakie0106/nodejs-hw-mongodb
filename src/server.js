a2f98bb0a013a4755058c73be33a9714412d0a08
Jakie0106 <kamikadze777939@gmail.com>
2024-12-21 15:57:30 +0200

    hw-7-swagger

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import * as fs from 'node:fs';
+import path from 'node:path';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
@@ -8,8 +10,7 @@ import notFoundHandler from './middlewares/notFoundHandler.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
-import path from 'node:path';
-import fs from 'node:fs';
+import swaggerUi from 'swagger-ui-express';
 
 const TMP_DIR = path.resolve('src', 'tmp');
 if (!fs.existsSync(TMP_DIR)) {
@@ -19,10 +20,14 @@ if (!fs.existsSync(TMP_DIR)) {
 export const setupServer = () => {
   const app = express();
   const logger = pino();
+  const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve('docs/swagger.json'), 'utf-8'),
+  );
 
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
